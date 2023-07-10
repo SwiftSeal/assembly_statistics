7b54e92b870787221944af958a32d5ffda30f5b6
SwiftSeal <moraysmith98@gmail.com>
2023-07-10 21:40:17 +0100

    add histogram function

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ struct Summary {
     contig_count: u64,
     largest_scaffold: u64,
     largest_contig: u64,
+    sorted_contig_lengths: Vec<u64>,
 }
 
 impl std::fmt::Display for Scaffold {
@@ -78,16 +79,16 @@ fn unit_scaling(value: u64) -> String {
 }
 
 fn parse_scaffold(sequence: &[u8]) -> Scaffold {
-    let mut a = 0;
-    let mut t = 0;
-    let mut g = 0;
-    let mut c = 0;
-    let mut length = 0;
-    let mut contig_lengths = Vec::new();
-    let mut contig_gcs = Vec::new();
+    let mut a: u64 = 0;
+    let mut t: u64 = 0;
+    let mut g: u64 = 0;
+    let mut c: u64 = 0;
+    let mut length: u64 = 0;
+    let mut contig_lengths: Vec<u64> = Vec::new();
+    let mut contig_gcs: Vec<f64> = Vec::new();
 
-    let mut contig_length = 0;
-    let mut contig_gc = 0;
+    let mut contig_length: u64 = 0;
+    let mut contig_gc: i32 = 0;
 
     for base in sequence {
         match base {
@@ -143,12 +144,12 @@ fn summary_statistics(scaffolds: &[Scaffold]) -> Summary {
     // Scaffold statistics
     let scaffold_length: u64 = scaffolds.iter().map(|scaffold| scaffold.length).sum();
     let scaffold_count: u64 = scaffolds.len() as u64;
-    let largest_scaffold: u64 = scaffolds.iter().map(|scaffold| scaffold.length).max().unwrap_or(0);
+    let largest_scaffold: u64 = scaffolds.iter().map(|scaffold: &Scaffold| scaffold.length).max().unwrap_or(0);
 
     // get sorted list of all contig lengths
     let mut contig_lengths: Vec<u64> = scaffolds
         .iter()
-        .flat_map(|scaffold| scaffold.contig_lengths.clone())
+        .flat_map(|scaffold: &Scaffold| scaffold.contig_lengths.clone())
         .collect();
     
     contig_lengths.sort();
@@ -157,17 +158,17 @@ fn summary_statistics(scaffolds: &[Scaffold]) -> Summary {
 
     let contig_length: u64 = contig_lengths.iter().sum();
     let contig_count: u64 = contig_lengths.len() as u64;
-    let n50_threshold = (contig_length as f64 * 0.5) as u64;
-    let n90_threshold = (contig_length as f64 * 0.9) as u64;
+    let n50_threshold: u64 = (contig_length as f64 * 0.5) as u64;
+    let n90_threshold: u64 = (contig_length as f64 * 0.9) as u64;
 
     // iterate over contig lengths in descending order and calculate N/L50 and N/L90. Once N50 has been calculated it will not be recalculated
-    let mut accumulated_length = 0;
-    let mut number_of_contigs = 0;
+    let mut accumulated_length: u64 = 0;
+    let mut number_of_contigs: u64 = 0;
 
-    let mut n50 = 0;
-    let mut l50 = 0;
-    let mut n90 = 0;
-    let mut l90 = 0;
+    let mut n50: u64 = 0;
+    let mut l50: u64 = 0;
+    let mut n90: u64 = 0;
+    let mut l90: u64 = 0;
     for length in contig_lengths.iter().rev() {
         accumulated_length += length;
         number_of_contigs += 1;
@@ -195,28 +196,58 @@ fn summary_statistics(scaffolds: &[Scaffold]) -> Summary {
         contig_count,
         largest_scaffold,
         largest_contig,
+        sorted_contig_lengths: contig_lengths,
     }
+}
+
+fn length_histogram(sorted_contig_lengths: &Vec<u64>) {
+    // Take a sorted list of contig lengths and print a horizontal histogram
+    use termion::terminal_size;
+    let term_size = terminal_size().unwrap_or((0, 0));
+
+    // calculate bin size
+    let max_contig_length: f64 = *sorted_contig_lengths.last().unwrap_or(&0) as f64;
+    let n_bins: f64 = term_size.1 as f64;
+    let bin_size: f64 = max_contig_length / n_bins;
+
+    let mut bins: Vec<u64> = vec![0; n_bins as usize + 1];
+
+    for length in sorted_contig_lengths {
+        let bin: usize = (*length as f64 / bin_size) as usize;
+        bins[bin] += *length;
+    }
+
+    // scale bin sizes to terminal width
+    let max_bin_size: u64 = *bins.iter().max().unwrap_or(&0);
+    let scale_factor: f64 = term_size.0 as f64 / max_bin_size as f64;
+
+    for bin in bins {
+        let scaled_bin: u64 = (bin as f64 * scale_factor) as u64;
+        println!("{}", "*".repeat(scaled_bin as usize));
+    }
+
+    
 }
 
 fn main() {
     use bio::io::fasta;
     use std::io;
 
-    let reader = fasta::Reader::new(io::stdin());
+    let reader: fasta::Reader<io::BufReader<io::Stdin>> = fasta::Reader::new(io::stdin());
 
     let mut scaffolds: Vec<Scaffold> = Vec::new();
 
     for result in reader.records() {
-        let record = result.expect("Error during fasta record parsing");
+        let record: fasta::Record = result.expect("Error during fasta record parsing");
 
-        let scaffold = parse_scaffold(record.seq());
+        let scaffold: Scaffold = parse_scaffold(record.seq());
 
         scaffolds.push(scaffold);
     }
 
-    let summary = summary_statistics(&scaffolds);
+    let summary: Summary = summary_statistics(&scaffolds);
 
     println!("L/N50: {}/{}", summary.l50, unit_scaling(summary.n50));
     println!("L/N90: {}/{}", summary.l90, unit_scaling(summary.n90));
-
+    length_histogram(&summary.sorted_contig_lengths);
 }
